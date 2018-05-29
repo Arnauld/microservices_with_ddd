c8126c5b16606ac406a590678ca4fa4adbfbbce0
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2018-05-29 15:06:42 +0200

    Prise en compte de la quantité dans le calcul du poids d'un panier

--- bounded-contexts/java/src/main/java/p1/myshop/entities/Quantity.java
+++ bounded-contexts/java/src/main/java/p1/myshop/entities/Quantity.java
@@ -1,4 +1,14 @@
 package p1.myshop.entities;
 
 public class Quantity {
+
+    int value;
+
+    Quantity(int value) {
+        this.value = value;
+    }
+
+    public int toInt() {
+        return value;
+    }
 }
